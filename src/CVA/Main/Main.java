4a3265570705be4a5e7a816a1334759ba9a16e75
Earthsy <31551677+Earthsy@users.noreply.github.com>
2017-09-01 14:32:27 -0600

    Add files via upload

---
+++ src/CVA/Main/Main.java
@@ -0,0 +1,7 @@
+package CVA.Main;
+
+public class Main {
+	public static void main(String[] args) {
+		
+	}
+}
---
+++ src/CVA/Main/Strings.java
@@ -0,0 +1,8 @@
+package CVA.Main;
+
+public class Strings {
+	/*public static void Main() {
+	*	
+	}*/
+	//public static final String
+}
